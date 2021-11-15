2fb6dbd66811eefcc570840646d2308dd178b804
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-11-15 19:43:45 +0700

    chore(theme.config.js): :wrench: update the meta tag

--- theme.config.js
+++ theme.config.js
@@ -16,25 +16,40 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta
+        name="description"
+        content="This is my personal documentation site."
+      />
+      <meta
+        name="og:description"
+        content="This is my personal documentation site."
+      />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta
+        name="twitter:image"
+        content="https://yehez-og-image.yehezgun.com/**docs**.yehezgun.com.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1636202181%2Fpeep_amkhuu.svg&widths=250&heights=250"
+      />
+      <meta name="twitter:site:domain" content="docs.yehezgun.com" />
+      <meta name="twitter:url" content="https://docs.yehezgun.com" />
+      <meta name="og:title" content="Yehezkiel Gunawan's Personal Docs Site" />
+      <meta
+        name="og:image"
+        content="https://yehez-og-image.yehezgun.com/**docs**.yehezgun.com.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1636202181%2Fpeep_amkhuu.svg&widths=250&heights=250"
+      />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Yehezkiel Gunawan's Personal Docs Site"
+      />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-icon-180x180.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="/android-chrome-icon.png"
       />
       <link
         rel="icon"
@@ -62,6 +77,13 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © <a href="https://yehezgun.com" target="_blank">Yehezkiel Gunawan</a></>,
+  footerText: (
+    <>
+      MIT {new Date().getFullYear()} ©{' '}
+      <a href="https://yehezgun.com" target="_blank">
+        Yehezkiel Gunawan
+      </a>
+    </>
+  ),
   unstable_faviconGlyph: '📗',
 }
