de2fc25384fbfa1606536387d77a7cfb5e992e94
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-03-03 17:08:33 +0700

    feat: :sparkles: change page view structure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_staticImage: true,
+  unstable_stork: false,
 })
-module.exports = withNextra()
+module.exports = withNextra({
+  experimental: {
+    optimizeCSs: true,
+  },
+})
